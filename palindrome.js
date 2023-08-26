396b0e90d2747ca00d3999d21b40d44cf4a61239
nsdsahil <sahilroyal91@gmail.com>
2023-08-26 23:17:31 +0530

    new file created

---
+++ palindrome.js
@@ -0,0 +1,8 @@
+let str="sahil";
+let rev=""
+for(let i=str.length-1;i>=0;i++)
+{
+    rev+=str[i]
+}
+if(str==0) console.log("it is a palindrome")
+else console.log("it is not")
---
+++ prime.js
@@ -0,0 +1,8 @@
+let num=15;
+let count=0;
+for(let i=1;i<=15;i++)
+{
+    if(num%i==0) count--
+}
+if(count==2)console.log("prime")
+else console.log("not prime")
